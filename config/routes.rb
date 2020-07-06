a1d2856e03ad50a2d2db8a53343a36f5948d067b
prachi <prachikwatra7@gmail.com>
2020-07-07 01:41:18 +0530

    Added Courses listing and front-end styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'courses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'pages#about'
+  resources 'courses'
 end
