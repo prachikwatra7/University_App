95a5014ebf26aa26333fd54cc4dd2b493b4d50ee
prachi <prachikwatra7@gmail.com>
2020-07-09 00:36:45 +0530

    Added students resource and it's  associations with courses

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+class StudentsController < ApplicationController
+  skip_before_action :require_user, only: [:new, :create]
+  before_action :set_student, only: [:show, :edit, :update]
+  before_action :require_same_student, only: [:edit, :update]
+
+  def show
+    
+  end
+
+  def edit
+  end
+
+  def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:notice] = 'You have successfully signed up'
+      redirect_to @student
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @student.update(student_params)
+      flash[:notice] = 'You have successfully updated your profile'
+      redirect_to @student
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :email, :password)
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def require_same_student
+    if current_user != @student
+      flash[:notice] = "You can only edit your own profile"
+      redirect_to student_path(current_user)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about', to: 'pages#about'
   resources 'courses'
+  resources 'students', except: [:destroy]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  post 'course_enroll', to: 'student_courses#create'
 end
